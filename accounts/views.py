7b4d6f1f78b6da9efb54fc0aaf43da1461492a2b
Amirbek <61391976+AAmir007-code@users.noreply.github.com>
2023-07-31 10:22:47 -0400

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import CreateView
-from django.contrib.auth.forms import BaseUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 # from .forms import RegisterForm
 
@@ -8,6 +8,6 @@ from django.urls import reverse_lazy
 
 
 class RegisterView(CreateView):
-    form_class = BaseUserCreationForm
+    form_class = UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
